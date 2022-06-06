f9009a5bc2a886d176191e52eb6ff59eff466085
Dawid-Dahl <dawid.dahl@kreationsbyran.se>
2022-06-06 18:14:07 +0200

    added drawer

--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from "react-native"
+import { Button, StyleSheet, Text, View } from "react-native"
 import React from "react"
 
 type Props = {}
 
-const UserScreen: React.FC<Props> = ({}: any) => {
+const UserScreen: React.FC<Props> = ({ navigation }: any) => {
+	const openDrawerHandler = () => {
+		navigation.toggleDrawer()
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text>UserScreen</Text>
+			<Button title="Open Drawer" onPress={openDrawerHandler}></Button>
 		</View>
 	)
 }
